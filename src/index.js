928576e5b0c63634ad70dbd34094c53a88b390b3
张强 <duwan.zq@gmail.com>
2023-03-15 08:35:40 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,250 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { IconHelpCircle, IconFeishuLogo, IconLanguage, IconBell, IconTiktokLogo, IconVigoLogo, IconXiguaLogo, IconTopbuzzLogo, IconFaceuLogo, IconPipixiaLogo, IconBytedanceLogo, IconDongchediLogo, IconGlobe, IconBranch, IconFile, IconFolderOpen, IconArrowRight, IconTickCircle } from "@douyinfe/semi-icons";
+import "./normalize.css";
+import styles from "./index.module.scss";
+
+const Component = () => {
+  return (
+    <div className={styles.landingPage}>
+    <div className={styles.frame1007}>
+        <div className={styles.frame1006}><img className={styles.union} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/44eed73a62ab4921_1665567139491.png" />
+            <p className={styles.xXX}>XXX</p>
+        </div>
+        <div className={styles.frame237}>
+            <div className={styles.navTitle}>
+                <p className={styles.text}>文案管理</p>
+            </div>
+            <div className={styles.navTitle}>
+                <p className={styles.text}>文档翻译</p>
+            </div>
+            <div className={styles.navTitle}>
+                <p className={styles.text_369be349}>语料库</p>
+                <div className={styles.badgeDangerSolid}>
+                    <p className={styles.number}>New</p>
+                </div>
+            </div>
+        </div><img className={styles.frame1005} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/b2d35c286d82e445_1665567139595.png" />
+    </div>
+    <div className={styles.frame731}>
+        <div className={styles.rectangle333}></div><img className={styles.onlineWorldAmico1} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/d948da3e9e0e987b_1665567139889.png" />
+        <p className={styles.text_e9687cca}>产品国际化一站式解决方案</p>
+        <p className={styles.text_096efa5c}>XXX&nbsp;是一个智能国际化翻译平台，为团队提供高效专业的【平台+服务】解决方案，简化本地化管理流程，提高多语言内容管理效率，助力产品国际化转型。</p>
+        <div className={styles.group}>
+            <div className={styles.solidBlockTextPrimar}>
+                <div className={styles.rectangle}></div>
+                <p className={styles.text_c5c91808}>立即使用</p>
+            </div>
+        </div>
+        <div className={styles.rectangle_3a438062}></div>
+        <p className={styles.text_ad22f185}>快速入门</p><img className={styles.group199} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/a39f62780c0e2b2c_1665567142595.png" />
+        <p className={styles.text_db084193}>60+&nbsp;业务线已经接入&nbsp;XXX</p>
+    </div>
+    <div className={styles.frame731}>
+        <div className={styles.rectangle333}></div>
+        <div className={styles.rectangle10}></div>
+        <p className={styles.text_cf8ddd15}>立即使用</p>
+        <p className={styles.text_7e246147}>支持多格式文档翻译，专业译员及时响应&nbsp;</p>
+        <div className={styles.group20}>
+            <p className={styles.text_60d0dacd}>文档翻译</p>
+        </div>
+        <p className={styles.text_380121f5}>文案管理</p>
+        <p className={styles.text_55efd2e0}>协同管理产品文案，一键发布快速生效</p>
+        <p className={styles.text_e93ed7f9}>立即使用</p><img className={styles.documentsAmico11} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/331d6785180eda12_1665567141146.png" /><img className={styles.workChatPana11} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/89f8ed1268f3a183_1665567143310.png" />
+    </div>
+    <div className={styles.frame731}>
+        <div className={styles.rectangle333}></div>
+        <div className={styles.rectangle42}></div>
+        <p className={styles.text_3cf4994e}>多语言管理</p>
+        <p className={styles.text_84a126f7}>独立的文案管理空间，多端同步，一键下发，快速上线</p>
+        <p className={styles.text_77cf3c93}>多场景翻译</p>
+        <p className={styles.text_f7d0ed88}>支持文案及文档翻译，拥有强大的编辑功能和丰富的翻译资源</p>
+        <p className={styles.text_d5c22521}>安全协作</p>
+        <p className={styles.text_219606e9}>支持多人协作，细粒度划分成员权限，有效保障项目安全。</p>
+        <p className={styles.text_eb1a16dc}>为你的团队高效管理多语言内容</p>
+        <p className={styles.text_bd7302fd}>XXX&nbsp;将&nbsp;A&nbsp;I技术、内容、人员融合到一起，简化本地化项目管理过程，提升本地化效率和质量</p>
+        <p className={styles.text_b838d0d2}>了解更多</p><img className={styles.group369} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/ffbbb8c6d77be03f_1665567144303.png" />
+    </div>
+    <div className={styles.frame734}>
+        <p className={styles.text_58f6b99e}>XXX&nbsp;的工具与资源</p>
+        <div className={styles.autoWrapper}>
+            <div className={styles.frame830}>
+                <div className={styles.group39}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/5ac90654ad3819b7_1665567141885.png" />
+                    <div className={styles.autoWrapper_e66f07e5}>
+                        <p className={styles.text_47a5230c}><span className={styles.text_7c292878}>命令行工具</span><span className={styles.text_e5373332}>&nbsp;XXX-cli</span></p>
+                        <p className={styles.text_2bb1fa73}>支持文件、注释和项目的快速国际化</p>
+                        <p className={styles.text_a71b8af3}>使用帮助</p>
+                    </div>
+                </div><img className={styles.group3} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/550af0f2525424d5_1665567141885.png" />
+            </div>
+            <div className={styles.frame831}>
+                <div className={styles.group40}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/5eb454399c0d573f_1665567143948.png" />
+                    <div className={styles.group37}>
+                        <p className={styles.text_e3571755}><span className={styles.text_7c292878}>文案拉取&nbsp;</span><span className={styles.text_e5373332}>SDK</span></p>
+                        <p className={styles.text_d87c01af}>Web、Go和Python等SDK快速拉取文案</p>
+                        <div className={styles.frame843}>
+                            <p className={styles.webSDK}>Web&nbsp;SDK</p>
+                            <p className={styles.goSDK}>Go&nbsp;SDK</p>
+                            <p className={styles.pythonSDK}>Python&nbsp;SDK</p>
+                        </div>
+                    </div>
+                </div>
+                <IconLanguage className={styles.semiIconsLanguage} />
+            </div>
+        </div>
+        <div className={styles.autoWrapper_6d7ba691}>
+            <div className={styles.frame832}>
+                <div className={styles.group42}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/fa1d6642264896cd_1665567143968.png" />
+                    <div className={styles.autoWrapper_e66f07e5}>
+                        <p className={styles.xXXIntl}>XXX-intl</p>
+                        <p className={styles.text_a45207ad}>字符串，时间，日期等内容的&nbsp;i18n&nbsp;工具</p>
+                        <p className={styles.text_dc948699}>使用帮助</p>
+                    </div>
+                </div>
+                <IconGlobe className={styles.semiIconsLanguage} />
+            </div>
+            <div className={styles.frame833}>
+                <div className={styles.group44}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/1da503d6bfd33adf_1665567144289.png" />
+                    <div className={styles.group34}>
+                        <p className={styles.xXXVscode}>XXX-vscode</p>
+                        <p className={styles.text_a01a1f55}>代码高亮、文案查询等辅助&nbsp;i18n&nbsp;插件</p>
+                        <p className={styles.text_a71b8af3}>使用帮助</p>
+                    </div>
+                </div><img className={styles.code} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/83590f84525f14f6_1665567143950.png" />
+            </div>
+        </div>
+        <div className={styles.autoWrapper_6d7ba691}>
+            <div className={styles.frame834}>
+                <div className={styles.group42_5d7d529b}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/cf1f179ee502a0a1_1665567143945.png" />
+                    <div className={styles.autoWrapper_e66f07e5}>
+                        <p className={styles.openAPI}>Open&nbsp;API</p>
+                        <p className={styles.text_869ff55c}>通过&nbsp;API&nbsp;集成&nbsp;XXX&nbsp;到你的工作流</p>
+                        <p className={styles.text_dc948699}>使用帮助</p>
+                    </div>
+                </div>
+                <IconBranch className={styles.semiIconsLanguage} />
+            </div>
+            <div className={styles.frame835}>
+                <div className={styles.group44_0a25ea67}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/d0f297b887321ac2_1665567143920.png" />
+                    <div className={styles.group34}>
+                        <p className={styles.webhooks}>Webhooks</p>
+                        <p className={styles.text_cba8da73}>通过通知外部服务如&nbsp;API，来自定义通知流</p>
+                        <p className={styles.text_a71b8af3}>使用帮助</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className={styles.autoWrapper_6d7ba691}>
+            <div className={styles.frame836}>
+                <div className={styles.group42_a77b54b5}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/a281a531d0ad0533_1665567143354.png" />
+                    <div className={styles.autoWrapper_e66f07e5}>
+                        <p className={styles.fileSupports}>File&nbsp;Supports</p>
+                        <p className={styles.text_fbb8b5ef}>支持解析xlsx，json，strings等多种文件类型</p>
+                        <p className={styles.text_dc948699}>使用帮助</p>
+                    </div>
+                </div>
+                <IconFile className={styles.semiIconsLanguage} />
+            </div>
+            <div className={styles.frame837}>
+                <div className={styles.group44_e24ea6cf}><img className={styles.group38} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/590c7577907e855f_1665567142721.png" />
+                    <div className={styles.group34}>
+                        <p className={styles.inContextEditor}>In-context&nbsp;Editor</p>
+                        <p className={styles.text_9a6f6a04}>基于真实应用和文案场景实现文案动态翻译</p>
+                        <div className={styles.autoWrapper_cb397127}>
+                            <p className={styles.text_cc51e88b}>下载插件</p>
+                            <p className={styles.text_cc51e88b}>使用帮助</p>
+                        </div>
+                    </div>
+                </div><img className={styles.group3} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/40caaee9f8f4ee22_1665567143967.png" />
+            </div>
+        </div>
+    </div>
+    <div className={styles.frame732}>
+        <div className={styles.rectangle46}></div>
+        <p className={styles.text_95747343}>我们是如何工作的</p>
+        <div className={styles.group196}>
+            <div className={styles.group195}>
+                <div className={styles.autoWrapper_2d4ec810}>
+                    <div className={styles.autoWrapper_1b657944}>
+                        <div className={styles.ellipse5}></div>
+                        <IconFolderOpen className={styles.semiIconsFolderOpen} />
+                    </div>
+                    <p className={styles.text_42f6e8bf}>创建项目</p>
+                </div>
+                <IconArrowRight className={styles.semiIconsArrowRight} />
+                <div className={styles.autoWrapper_2d4ec810}>
+                    <div className={styles.autoWrapper_1b657944}>
+                        <div className={styles.ellipse6}></div><img className={styles.cloud} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/b88a541c9e3b6496_1665567142525.png" />
+                    </div>
+                    <p className={styles.text_2fa2f509}>整合内容</p>
+                </div>
+                <IconArrowRight className={styles.semiIconsArrowRight} />
+                <div className={styles.autoWrapper_2d4ec810}>
+                    <div className={styles.autoWrapper_1b657944}>
+                        <div className={styles.ellipse7}></div><img className={styles.cloud} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/f01beabc3f8bbab7_1665567142539.png" />
+                    </div>
+                    <p className={styles.text_2fa2f509}>翻译管理</p>
+                </div>
+                <IconArrowRight className={styles.semiIconsArrowRight} />
+                <div className={styles.autoWrapper_2d4ec810}>
+                    <div className={styles.autoWrapper_1b657944}>
+                        <div className={styles.ellipse8}></div>
+                        <IconTickCircle className={styles.semiIconsFolderOpen} />
+                    </div>
+                    <p className={styles.text_2fa2f509}>一键下发</p>
+                </div>
+            </div>
+        </div>
+        <div className={styles.ellipse19}></div>
+    </div>
+    <div className={styles.frame732}>
+        <div className={styles.rectangle46}></div><img className={styles.rectangle_f66a95da} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/4c51a0cc265e7544_1665567141907.png" />
+        <div className={styles.frame724}>
+            <div className={styles.ellipse17}></div>
+            <div className={styles.frame722}>
+                <p className={styles.text_e0161644}>译员端</p>
+                <p className={styles.text_28545dbc}>XXX&nbsp;译员端拥有丰富的翻译资源，目前已支持&nbsp;80+&nbsp;的翻译语言，为用户的翻译任务保驾护航。</p>
+                <p className={styles.text_2e1e83f5}>立即使用</p>
+            </div>
+        </div>
+    </div>
+    <div className={styles.frame732}>
+        <div className={styles.rectangle46}></div>
+        <p className={styles.text_8981d70f}>核心功能</p><img className={styles.rectangle_d0215024} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/f80a1b6a73113566_1665567141922.png" />
+        <div className={styles.frame723}>
+            <div className={styles.ellipse18}></div>
+            <div className={styles.frame721}>
+                <p className={styles.text_b50c3058}>可视化编辑器</p>
+                <p className={styles.text_e1853419}>XXX&nbsp;可视化编辑器是一款强大的在线翻译工具，基于真实应用程序和文案场景，实现翻译所见即所得。</p>
+            </div>
+        </div>
+        <p className={styles.text_a08d99df}>立即使用</p>
+    </div>
+    <div className={styles.frame733}>
+        <div className={styles.rectangle46_0d915c26}></div>
+        <div className={styles.frame726}>
+            <p className={styles.text_18611d7e}>XXX&nbsp;期待你的加入</p>
+            <div className={styles.group_83545f00}>
+                <div className={styles.solidBlockTextPrimar}>
+                    <div className={styles.rectangle_cdd811ff}></div>
+                    <p className={styles.text_b07ae722}>立即使用</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div className={styles.footer1}><img className={styles.brandingBnW} src="https://lf3-files.qingfuwucdn.net/obj/inspirecloud-file/baas/tt38q7/e4eed64f1dc783bc_1665567141903.png" />
+        <p className={styles.text_0fce1d9e}>©&nbsp;2012-2020&nbsp;字节跳动</p>
+        <div className={styles.link}>
+            <p className={styles.text_8d382d86}>更新日志</p>
+            <p className={styles.text_8d382d86}>帮助文档</p>
+            <p className={styles.text_8d382d86}>平台客服</p>
+            <p className={styles.text_8d382d86}>反馈建议</p>
+        </div>
+    </div>
+</div>
+  )
+}
+
+const dom = document.querySelector("#root");
+ReactDOM.render(<div style={{width: 1440, height: 5020}}><Component /></div>, dom);
